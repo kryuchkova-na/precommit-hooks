22ae5795880fa28245d3147ae6c460f7db4ee1fe
Nataliya Kryuchkova <kryuchkova.na@yandex.ru>
2025-05-22 15:09:07 +0400

    run linters

--- precommit_hooks/cli.py
+++ precommit_hooks/cli.py
@@ -4,12 +4,29 @@ Command-line interface for precommit-hooks.
 This module provides the CLI functionality using Click.
 """
 
-import click
 import subprocess
+
+import click
+
+# if $FIX_MODE; then
+#   echo "Fixing formatting..."
+#   poetry run black ./
+#   poetry run isort ./
+#   poetry run ruff check --fix
+# else
+#   echo "Checking formatting..."
+#   black --check .
+#   isort --check-only .
+#   ruff check .
+# fi
+#
 
 
 def run_command(cmd, *options):
-    subprocess.run(["poetry", "run", cmd, *options])
+    click.echo(f"Running {cmd} ")
+    cleaned_options = list(filter(lambda x: x != "", options))
+    # click.echo(["poetry", "run", cmd, *options])
+    subprocess.run(["poetry", "run", cmd, *cleaned_options])
 
 
 @click.group()
@@ -20,11 +37,13 @@ def cli():
 
 
 @cli.command()
-@click.option("--fix", "fix")
+@click.option("--fix/--no-fix", "fix")
 @click.argument("path", default=".")
 def codestyle(fix: bool, path: str):
     click.echo("Running code formatters...")
-    run_command("black", path)
+    run_command("black", path, "" if fix else "--check")
+    run_command("isort", path, "" if fix else "--check-only")
+    run_command("ruff", "check", path, "--fix" if fix else "")
 
 
 if __name__ == "__main__":
